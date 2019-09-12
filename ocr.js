a24a1f49bc85820cfad464b0eca4be6b46322a3d
Stratos Giouldasis <giouldasis.stratos@gmail.com>
2019-09-12 17:05:28 +0300

    Resilient to missing data

--- ocr.js
+++ ocr.js
@@ -4,35 +4,48 @@ const Tesseract = require('tesseract.js');
 const Jimp = require('jimp');
 
 // Provision the path
-const filePathArgument = process.argv[2] || null;
-if (!filePathArgument) failure('Path is missing');
+const filePath = process.argv[2] || null;
+if (!filePath) {
+  console.error('Path is missing');
+  process.exit(1);
+}
 
 // Clean the path
-const filePathArgumentSplit = filePathArgument.split('.');
-const filePathWithoutExtension = filePathArgumentSplit && filePathArgumentSplit[0] ? filePathArgumentSplit[0] : null;
-if (!filePathWithoutExtension) failure('Failed to identify path to file without extension');
+const filePathSplit = filePath.split('.');
+const fileName = filePathSplit && filePathSplit[0] ? filePathSplit[0] : null;
+if (!fileName) {
+  console.error('Failed to identify path to file without extension');
+  process.exit(1);
+}
+
+if (!fs.existsSync(`${fileName}.gif`)) {
+  console.error(`File ${fileName}.gif does not exist`);
+  process.exit(1);
+}
 
 function success(res) {
   if (res && res.text) {
     console.log(`|||${res.text.replace(/\D/g, '')}|||`);
   }
-  clean();
+  fs.unlinkSync(`${fileName}.gif`)
+  fs.unlinkSync(`${fileName}.jpg`)
   process.exit(0);
 }
 
 function failure(err) {
   console.error(err);
-  clean();
+  fs.unlinkSync(`${fileName}.gif`)
+  fs.unlinkSync(`${fileName}.jpg`)
   process.exit(1);
 }
 
-function clean() {
-  fs.unlinkSync(`${filePathWithoutExtension}.gif`)
-  fs.unlinkSync(`${filePathWithoutExtension}.jpg`)
-}
-
 // Execute
-Jimp.read(`${filePathWithoutExtension}.gif`)
-  .then((img) => img.background(0xFFFFFFFF).contrast(-0.5).scale(1.5).quality(100).write(`${filePathWithoutExtension}.jpg`))
-  .then(() => Tesseract.recognize(`${filePathWithoutExtension}.jpg`).then(success).catch(failure))
-  .catch(failure);
+Jimp.read(`${fileName}.gif`)
+  .then((img) => img.background(0xFFFFFFFF).contrast(-0.5).scale(1.5).quality(100).write(`${fileName}.jpg`))
+  .then(() => {
+    Tesseract
+      .recognize(`${fileName}.jpg`)
+      .then(success)
+      .catch(failure)
+  })
+  .catch(failure)
