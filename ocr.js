6b6c17350ccf4d8eb57befe1ee1490f3914e77ef
Stratos Giouldasis <giouldasis.stratos@gmail.com>
2019-09-12 15:56:05 +0300

    OCR is reading a gif file now

--- ocr.js
+++ ocr.js
@@ -1,24 +1,35 @@
 const process = require('process');
 const Tesseract = require('tesseract.js');
+const Jimp = require('jimp');
 
-const filePath = process.argv[2] || null;
+function success(res) {
+  if (res && res.text) {
+    console.log(`<${res.text.replace(/\D/g, '')}>`);
+  }
 
-if (!filePath) {
-  console.error('Filepath is missing');
-  process.exit(1);
-}
-
-function exitOK(data) {
-  console.log(`<${data}>`);
   process.exit(0);
 }
 
-Tesseract.recognize(filePath)
-  .then(({ text }) => {
-    const cleanedResult = text.replace(/\D/g, '');
-    exitOK(cleanedResult)
-  })
-  .finally((resultOrError) => console.log(resultOrError))
-  .catch(err => console.error('test1', err));
+function failure(err) {
+  console.error(err);
+  process.exit(1);
+}
 
+function getFilePathWithoutExtension(filePath) {
+  const splitting = filePath.split('.');
+  return splitting && splitting[0] ? splitting[0] : null;
+}
 
+// Provision the path
+const filePathArgument = process.argv[2] || null;
+if (!filePathArgument) failure('Path is missing');
+
+// Clean the path
+const filePathWithoutExtension = getFilePathWithoutExtension(filePathArgument);
+if (!filePathWithoutExtension) failure('Failed to identify path to file without extension');
+
+// Execute
+Jimp.read(`${filePathWithoutExtension}.gif`)
+  .then((img) => img.background(0xFFFFFFFF).contrast(-0.5).quality(100).write(`${filePathWithoutExtension}.jpg`))
+  .then(() => Tesseract.recognize(`${filePathWithoutExtension}.jpg`).then(success).catch(failure))
+  .catch(failure);
